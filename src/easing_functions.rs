63b2738d34cd4bc7943b2b7b6207cbce6e769c97
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-03-20 14:43:08 +0900

    update

--- src/easing_functions.rs
+++ src/easing_functions.rs
@@ -1,33 +1,48 @@
 macro_rules! ease {
-    ($mod_name: ident, $ease_in:expr) => {
-    // ($mod_name: ident, $ease_in:expr, $ease_out:expr, $ease_in_out:expr) => {
+    ($mod_name: ident, $ease_in:expr, $ease_out:expr, $ease_in_out:expr) => {
         pub mod $mod_name {
-        use num_traits::{Float, FloatConst, FromPrimitive};
+            use num_traits::{Float, FloatConst, FromPrimitive};
             #[inline]
             pub fn ease_in<T: Float + FloatConst + FromPrimitive>(x: T) -> T {
                 $ease_in(x)
             }
-            // #[inline]
-            // pub fn ease_out<T: Float>(x: T) -> T {
-            //     $ease_out(x)
-            // }
-            // #[inline]
-            // pub fn ease_in_out<T: Float>(x: T) -> T {
-            //     $ease_in_out(x)
-            // }
+            #[inline]
+            pub fn ease_out<T: Float + FloatConst + FromPrimitive>(x: T) -> T {
+                $ease_out(x)
+            }
+            #[inline]
+            pub fn ease_in_out<T: Float + FloatConst + FromPrimitive>(x: T) -> T {
+                $ease_in_out(x)
+            }
         }
     }
 }
 
-use num_traits::{Float, FloatConst, FromPrimitive};
-pub fn f<T: Float + FloatConst + FromPrimitive>(x: f64) -> T {
-    T::from(x).unwrap()
-}
 ease!(sine,
-    |x| crate::f::<T>(1.0) - T::cos((x * T::PI()) / crate::f::<T>(2.0))
+    |x| T::from(1).unwrap() - T::cos(x * T::PI() / T::from(2).unwrap()),
+    |x| T::sin(x * T::PI() / T::from(2).unwrap()),
+    |x| -(T::cos(x * T::PI()) - T::from(1).unwrap()) / T::from(2).unwrap(),
 );
-// ease!(cubic)
-// ease!(quint)
+ease!(cubic,
+    |x| x.powi(3u32),
+    |x| T::from(1).unwrap() - (T::from(1).unwrap() - x).powi(3u32),
+    |x| if x < T::from(0.5).unwrap() {
+        T::from(4).unwrap() * x.powi(3u32)
+    } else {
+        T::from(1).unwrap() - (-T::from(2).unwrap() * x + 2).powi(3u32) / T::from(2).unwrap()
+    },
+);
+ease!(quint,
+    |x| x.powi(5u32),
+    |x| T::from(1).unwrap() - (T::from(1).unwrap() - x).powi(5u32),
+    |x| if x < T::from(0.5).unwrap() {
+        T::from(16).unwrap() * x.powi(3u32)
+    } else {
+        T::from(1).unwrap() - (-T::from(2).unwrap() * x + 2).powi(5u32) / T::from(2).unwrap()
+    },
+);
+ease!(circ,
+    |x| T::from(1).unwrap() - T::sqrt(T::from(1).unwrap()))
 // ease!(circ)
 // ease!(elastic)
 // ease!(quad)
