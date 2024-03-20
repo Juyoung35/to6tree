b107e3f67fe0ca2bd86272f7e57099f4b9548181
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-03-21 07:58:31 +0900

    update

--- src/easing_functions.rs
+++ src/easing_functions.rs
@@ -17,10 +17,74 @@ macro_rules! ease {
         }
     }
 }
+fn if_zero_one_else<T, C>(x: T, closure: C) -> T
+where
+    T: PartialEq<Float>,
+    C: Fn(T) -> T,
+{
+    if x == 0.0 | x == 1.0 {
+        x
+    } else {
+        closure(x)
+    }
+}
 
 ease!(sine,
     |x: T| 1.0 - cos(PI * x / 2.0),
     |x: T| sin(PI * x / 2.0),
     |x: T| -(cos(PI * x) - 1.0) / 2.0,
 );
-ease!()
+ease!(cubic,
+    |x: T| x.powi(3),
+    |x: T| 1.0 - (1.0 - x).powi(3),
+    |x: T| if x < 0.5 {
+        4.0 * x.powi(3)
+    } else {
+        1.0 - (-2.0 * x + 2.0).powi(3) / 2.0
+    },
+);
+ease!(quint,
+    |x: T| x.powi(5),
+    |x: T| 1.0 - (1.0 - x).powi(5),
+    |x: T| if x < 0.5 {
+        16.0 * x.powi(5)
+    } else {
+        1.0 - (-2.0 * x + 2.0).powi(5) / 2.0
+    },
+);
+ease!(circ
+    |x: T| 1.0 - sqrt(1.0 - x.powi(2)),
+    |x: T| sqrt(1.0 - (x - 1.0).powi(2)),
+    |x: T| if x < 0.5 {
+        1.0 - sqrt(1.0 - (2.0 * x).powi(2))
+    } else {
+        sqrt(1.0 - (-2.0 * x + 2.0).powi(2)) + 1.0
+    } / 2.0
+);
+ease!(elastic,
+    |x: T| if_zero_one_else(x, |y| -2.0.powf(10.0 * y - 10.0) * sin((10.0 * y - 10.75) * 2.0 * PI / 3.0)),
+    |x: T| if_zero_one_else(x, |y| 2.0.powf(-10.0 * y) * sin ((10.0 * y - 10.75) * 2.0 * PI / 3.0) + 1.0),
+    |x: T| if_zero_one_else(x, if x < 0.5 {
+        |y| -2.0.powf(20.0 * y - 10.0) * sin((20.0 * y - 11.125) * 2.0 * PI / 4.5) / 2.0
+    } else {
+        |y| 2.0.powf(-20.0 * y) * sin ((20.0 * y - 11.125) * 2.0 * PI / 4.5) / 2.0 + 1.0
+    }),
+);
+ease!(cubic,
+    |x: T| x.powi(2),
+    |x: T| 1.0 - (1.0 - x).powi(2),
+    |x: T| if x < 0.5 {
+        2.0 * x.powi(2)
+    } else {
+        1.0 - (-2.0 * x + 2.0).powi(2) / 2.0
+    },
+);
+ease!(quart,
+    |x: T| x.powi(2),
+    |x: T| 1.0 - (1.0 - x).powi(2),
+    |x: T| if x < 0.5 {
+        2.0 * x.powi(2)
+    } else {
+        1.0 - (-2.0 * x + 2.0).powi(2) / 2.0
+    },
+);
