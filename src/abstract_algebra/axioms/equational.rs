2128caba2399d9de1dd39580b6ab3f9236a4e908
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-03-22 09:50:59 +0900

    update

---
+++ src/abstract_algebra/axioms/equational.rs
@@ -0,0 +1,19 @@
+trait Commutativity<T, O> {
+
+}
+
+trait Associativity<T, O> {
+
+}
+
+trait LeftDistrubutivity<T, O> {
+
+}
+
+trait RightDistrubutivity<T, O> {
+    
+}
+
+trait Distrubutivity<T, O>: LeftDistrubutivity<T, O> + RightDistrubutivity<T, O> {
+    
+}
